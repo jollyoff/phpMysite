5601290f6c21e0e21baeecbd9750da07b830d77d
jollyoff <79255825+jollyoff@users.noreply.github.com>
2023-02-14 00:07:37 +0300

    Add files via upload

---
+++ templates/errors/401.php
@@ -0,0 +1,4 @@
+<?php include __DIR__ . '/../header.php'; ?>
+    <h1>We don`t authenticate you</h1>
+    For access to this page you need to be a <a href="/users/login">login</a>
+<?php include __DIR__ . '/../footer.php'; ?>
---
+++ templates/errors/500.php
@@ -0,0 +1,2 @@
+<h1>Houston, we have a problems 500!</h1>
+<?= $error ?>
