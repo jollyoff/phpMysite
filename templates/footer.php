3946b074e31a5c58706cba9da961d688941b4917
jollyoff <79255825+jollyoff@users.noreply.github.com>
2023-02-24 20:53:12 +0300

    Add files via upload

--- templates/footer.php
+++ templates/footer.php
@@ -1,26 +1,64 @@
-</td>
-<?php
-/**
- * @var \MyProject\Models\Users\User $user
- */
-?>
-<td width="300px" class="sidebar" bgcolor="#d8bfd8">
-    <div class="sidebarHeader">Menu</div>
-    <ul>
-        <?php if ($user!==null):?>
-        <li><a href="/">Main page</a></li>
-        <li><a href="/articles/add">Add News</a></li>
-        <?php endif?>
-        <?php if ($user!==null && $user->isAdmin()): ?>
-        <li><a href="/users/main">Users</a></li>
-        <?php endif?>
-    </ul>
-</td>
-</tr>
-<tr bgcolor="#d8bfd8">
-    <td class="footer" colspan="2">All news here</td>
-</tr>
-</table>
+<div class="wrapper">
+    <footer>
+        <nav class="navbar-footer">
+            <div class="container-footer">
+                <p class="footer">ALL NEWS HERE</p>
+                <div class="navbar-wrap-footer">
+                    <ul class="navbar-menu-footer">
+                        <div class="dropdown">
+                            <button onclick="myFunction1()" class="dropbtn">Telephone</button>
+                            <div id="myDropdown0" class="dropdown-content">
+                                <li><a type="number">+380950123112</a></li>
+                            </div>
+                            <button onclick="myFunction2()" class="dropbtn">E-mail</button>
+                            <div id="myDropdown1" class="dropdown-content">
+                                <li><a type="mail">allnewshere@mail.com</a></li>
+                            </div>
+                            <button onclick="myFunction3()" class="dropbtn">Social Media</button>
+                            <div id="myDropdown2" class="dropdown-content">
+                                <li><a href="http://instagram.com">Inst: NewsHere</a></li>
+                                <li><a href="http://facebook.com">Facebook: NewsHere</a></li>
+                                <li><a href="http://youtube.com">Youtube: NewsHere</a></li>
+                            </div>
+                            <button onclick="myFunction4()" class="dropbtn">Address</button>
+                            <div id="myDropdown3" class="dropdown-content">
+                                <li><a>Lutsk</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <script>
+            function myFunction1() {
+                document.getElementById("myDropdown0").classList.toggle("show");
+            }
+            function myFunction2() {
+                document.getElementById("myDropdown1").classList.toggle("show");
+            }
+            function myFunction3() {
+                document.getElementById("myDropdown2").classList.toggle("show");
+            }
+            function myFunction4() {
+                document.getElementById("myDropdown3").classList.toggle("show");
+            }
 
-</body>
-</html>
+            window.onclick = function(event) {
+                if (!event.target.matches('.dropbtn')) {
+
+                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                        }
+                        if (openDropdown.classList.contains('show') === true) {
+                            openDropdown.classList.remove('show');
+                        }
+                    }
+                }
+            }
+        </script>
+    </footer>
+</div>
--- templates/header.php
+++ templates/header.php
@@ -2,25 +2,112 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>php my site</title>
-    <link rel="stylesheet" href="/styles.css">
+    <title>PhpMySite</title>
+    <link rel="stylesheet" href="../styles.css">
 </head>
-<body bgcolor="#d8bfd8">
+<body>
 
-<table class="layout">
-    <tr bgcolor="#d8bfd8">
-        <td colspan="2" class="header">
-            <a href="/">News</a>
-        </td>
-    </tr>
-    <tr bgcolor="#d8bfd8">
-        <td colspan="2" style="text-align: right">
-            <?php if (!empty($user)) { ?>
-                Hello, <?= $user->getNickname() ?> | <a href="/users/logout">Exit</a>
-            <?php } else { ?>
-                <a href="/users/login">Sign in</a> | <a href="/users/register">Sign up</a>
-            <?php } ?>
-        </td>
-    </tr>
-    <tr bgcolor="#d8bfd8">
-        <td>
+<nav class="navbar">
+    <div class="container">
+        <a href="/" class="navbar-brand">News site</a>
+        <div class="navbar-wrap">
+            <ul class="navbar-menu">
+                <?php if (!empty($user)): ?>
+                    <li><a href="/articles/add">Add News</a></li>
+                        <?php if ($user->isAdmin()): ?>
+                            <li><a href="/users/main">Users</a></li>
+                        <?php endif;?>
+                <?php endif;?>
+            </ul>
+            <?php if (!empty($user)): ?>
+                <a href="/users/logout" class="exit">Exit</a>
+            <?php endif;?>
+            <?php if (empty($user)): ?>
+                <button id="registration-button" class="">Registration</button>
+                <div id="registration-window" class="popup">
+                    <div class="popup-content">
+                        <div class="popup-header">
+                            <span class="close">&times;</span>
+                        </div>
+                        <div class="popup-body">
+                            <div style="text-align: center;">
+                                <h1>Sign up</h1>
+                                <?php if (!empty($error)): ?>
+                                    <div style="background-color: red;padding: 5px;margin: 15px"><?= $error ?></div>
+                                <?php endif; ?>
+                                <form action="/users/register" method="post">
+                                    <label>Nickname <input type="text" name="nickname" value="<?= $_POST['nickname'] ?? '' ?>"></label>
+                                    <br><br>
+                                    <label>Email <input type="text" name="email" value="<?= $_POST['email'] ?? '' ?>"></label>
+                                    <br><br>
+                                    <label>Password <input type="password" name="password" value="<?= $_POST['password'] ?? '' ?>"></label>
+                                    <br><br>
+                                    <label>Password Confirm <input type="password" name="passwordconf" value="<?= $_POST['passwordconf'] ?? '' ?>"></label>
+                                    <br><br>
+                                    <input type="submit" value="Sign up">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button id="login-button" class="">Login</button>
+                <div id="login-window" class="popup-login">
+                    <div class="popup-content-login">
+                        <div class="popup-header-login">
+                            <span class="close-login">&times;</span>
+                        </div>
+                        <div class="popup-body-login">
+                            <h1>Sign in</h1>
+                            <form action="/users/login" method="post">
+                                <?php if (!empty($error)): ;?>
+                                    <div id="error" style="background-color: red;padding: 5px;margin: 15px"><?= $error ?></div>
+                                <?php endif; ?>
+                                <label>Email <input type="text" name="email" value="<?= $_POST['email'] ?? '' ?>"></label>
+                                <br><br>
+                                <label>Password <input type="password" name="password" value="<?= $_POST['password'] ?? '' ?>"></label>
+                                <br><br>
+                                <input id="submitButtonLogin" type="submit" value="Sign in">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            <?php endif;?>
+        </div>
+    </div>
+</nav>
+<script>
+    loginButton = document.getElementById("login-button");
+    loginWindow = document.getElementById("login-window");
+    loginClose = document.querySelector('.close-login');
+    registrationButton = document.getElementById("registration-button");
+    registrationWindow = document.getElementById("registration-window");
+    registrationClose = document.querySelector('.close');
+
+    loginButton.onclick = function (){
+        loginWindow.style.display = "block";
+    }
+
+    loginClose.onclick = function (){
+        loginWindow.style.display= "none";
+    }
+
+    registrationButton.onclick = function()
+    {
+        registrationWindow.style.display = "block";
+    };
+
+    registrationClose.onclick = function (){
+        registrationWindow.style.display = "none";
+    }
+
+    window.onclick = function(e) {
+        if(e.target === registrationWindow){
+            registrationWindow.style.display = "none";
+        }
+        if(e.target === loginWindow){
+            loginWindow.style.display = "none";
+        }
+    }
+</script>
+</body>
+</html>
