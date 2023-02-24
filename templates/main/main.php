9860c22932f10174b52ac4c1fd81c12bc1f9545d
jollyoff <79255825+jollyoff@users.noreply.github.com>
2023-02-24 20:52:19 +0300

    Add files via upload

--- templates/main/main.php
+++ templates/main/main.php
@@ -1,12 +1,35 @@
 <?php include __DIR__ . '/../header.php';
 /**
  * @var \MyProject\Models\Articles\Article $articles
- * @var \MyProject\Models\Users\User $users
+ * @var \MyProject\Models\Users\User $user
+ * @var \MyProject\Models\Comments\Comment $comment
+ * @var array $comments
  */
 ?>
+    <link rel="stylesheet" href="../../styles.css">
+    <body>
+    <div class="grid">
 <?php foreach ($articles as $article): ?>
-    <h2><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h2>
-    <p><?= $article->getText() ?></p>
-    <hr>
+    <div class="article-body">
+        <h2 class="article-title"><a href="/articles/<?= $article->getId() ?>">
+            <?= $article->getName() ?>
+            </a></h2>
+        <p class="article-content">
+            <?= $article->getText() ?>
+        </p>
+        <footer class="article-info">
+            <span>By <?= $article->getAuthor()->getNickname()?></span>
+            <span>
+                Comments:
+                <?php $number = $article->getNumberComments($article);
+                $commentsNumber = reset($number);
+                $count = $commentsNumber->{'cOUNT(*)'};
+                echo($count);
+                ?>
+            </span>
+        </footer>
+    </div>
 <?php endforeach; ?>
+    </div>
+    </body>
 <?php include __DIR__ . '/../footer.php'; ?>
